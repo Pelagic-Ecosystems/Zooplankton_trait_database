93257a4bd7f42b586abadab5de6773c8818b2049
Patrick Pata <patrickrpata@gmail.com>
2024-07-02 13:07:41 -0700

    modified app dev

--- shiny_app.R
+++
@@ -1,9 +0,0 @@
-# Create Shiny App
-# Created by P. Pata
-
-# install.packages("remotes")
-# remotes::install_github("rstudio/shinyuieditor")
-
-shinyuieditor::launch_editor(app_loc = here::here("ZTD_app/"))
-
-shiny::runApp("ZTD_app")
